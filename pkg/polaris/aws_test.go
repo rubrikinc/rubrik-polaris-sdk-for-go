e47c3ae6abf62116d276b4e221b2c1c2e8b030e5
Johan Andersson <johan.andersson@rubrik.com>
2021-04-08 14:29:40 +0200

    Test Jenkins pipeline

--- pkg/polaris/aws_test.go
+++ pkg/polaris/aws_test.go
@@ -9,30 +9,33 @@ import (
 	"github.com/trinity-team/rubrik-polaris-sdk-for-go/pkg/polaris/log"
 )
 
+// Between the account has been added and it has been removed we never fail
+// fatally to allow the account to be removed in case of an error.
 func TestAwsAccountAddAndRemove(t *testing.T) {
 	requireEnv(t, "SDK_INTEGRATION")
 
 	ctx := context.Background()
 
+	// Polaris client.
 	polConfig, err := DefaultConfig("default")
 	if err != nil {
 		t.Fatal(err)
 	}
-
 	client, err := NewClient(polConfig, &log.DiscardLogger{})
 	if err != nil {
 		t.Fatal(err)
 	}
 
+	// AWS config.
 	awsConfig, err := config.LoadDefaultConfig(ctx)
 	if err != nil {
 		t.Fatal(err)
 	}
 
-	if err := client.AwsAccountAdd(ctx, awsConfig, []string{"us-east-2", "us-west-2"}); err != nil {
+	// Add and verify AWS account.
+	if err := client.AwsAccountAdd(ctx, awsConfig, []string{"us-east-2"}); err != nil {
 		t.Fatal(err)
 	}
-
 	account, err := client.AwsAccountFromConfig(ctx, awsConfig)
 	if err != nil {
 		t.Error(err)
@@ -49,14 +52,34 @@ func TestAwsAccountAddAndRemove(t *testing.T) {
 	if name := account.Features[0].Feature; name != "CLOUD_NATIVE_PROTECTION" {
 		t.Errorf("invalid feature name, name=%v", name)
 	}
-	if regions := account.Features[0].AwsRegions; reflect.DeepEqual(regions, []string{"us-east-2", "us-west-2"}) {
+	if regions := account.Features[0].AwsRegions; reflect.DeepEqual(regions, []string{"us-east-2"}) {
 		t.Errorf("invalid feature regions, regions=%v", regions)
 	}
 	if status := account.Features[0].Status; status != "CONNECTED" {
 		t.Errorf("invalid feature status, status=%v", status)
 	}
 
+	// Set and verify regions for AWS account.
+	if err := client.AwsAccountSetRegions(ctx, account.NativeID, []string{"us-west-2"}); err != nil {
+		t.Error(err)
+	}
+	account, err = client.AwsAccountFromID(ctx, account.NativeID)
+	if err != nil {
+		t.Error(err)
+	}
+	if n := len(account.Features); n != 1 {
+		t.Errorf("invalid number of features, n=%v", n)
+	}
+	if regions := account.Features[0].AwsRegions; reflect.DeepEqual(regions, []string{"us-west-2"}) {
+		t.Errorf("invalid feature regions, regions=%v", regions)
+	}
+
+	// Remove AWS account and verify that it's gone.
 	if err := client.AwsAccountRemove(ctx, awsConfig, ""); err != nil {
 		t.Fatal(err)
+	}
+	account, err = client.AwsAccountFromConfig(ctx, awsConfig)
+	if err != ErrAccountNotFound {
+		t.Error(err)
 	}
 }
