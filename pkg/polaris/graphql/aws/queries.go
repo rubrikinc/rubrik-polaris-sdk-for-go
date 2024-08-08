83f650bfa4ffc18f463196a6fe01951e8b200aeb
Johan Andersson <johan.andersson@rubrik.com>
2024-08-08 10:15:55 +0200

    Add GraphQL query

--- pkg/polaris/graphql/aws/queries.go
+++ pkg/polaris/graphql/aws/queries.go
@@ -207,6 +207,17 @@ var awsExocomputeClusterConnectQuery = `mutation SdkGolangAwsExocomputeClusterCo
     }
 }`
 
+// awsExocomputeGetClusterConnectionInfo GraphQL query
+var awsExocomputeGetClusterConnectionInfoQuery = `query SdkGolangAwsExocomputeGetClusterConnectionInfo(clusterId: UUID!, $exocomputeConfigId: UUID!) {
+    awsExocomputeGetClusterConnectionInfo(input: {
+        clusterUuid:        $clusterId,
+        exocomputeConfigId: $exocomputeConfigId
+    }) {
+        clusterSetupYaml
+        connectionCommand
+    }
+}`
+
 // awsNativeAccount GraphQL query
 var awsNativeAccountQuery = `query SdkGolangAwsNativeAccount($awsNativeAccountRubrikId: UUID!, $awsNativeProtectionFeature: AwsNativeProtectionFeature!) {
 	awsNativeAccount(awsNativeAccountRubrikId: $awsNativeAccountRubrikId, awsNativeProtectionFeature: $awsNativeProtectionFeature) {
