35532c93485a74eba3c608f888a066421a500092
Andrew Draper <andrew.draper@rubrik.com>
2025-06-19 12:36:13 +0100

    Add CFT checks and outpost install

---
+++ examples/aws_account_with_outpost/main.go
@@ -0,0 +1,106 @@
+// Copyright 2021 Rubrik, Inc.
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to
+// deal in the Software without restriction, including without limitation the
+// rights to use, copy, modify, merge, publish, distribute, sublicense, and/or
+// sell copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+// FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+// DEALINGS IN THE SOFTWARE.
+
+package main
+
+import (
+	"context"
+	"fmt"
+	"log"
+
+	"github.com/rubrikinc/rubrik-polaris-sdk-for-go/pkg/polaris"
+	"github.com/rubrikinc/rubrik-polaris-sdk-for-go/pkg/polaris/aws"
+	"github.com/rubrikinc/rubrik-polaris-sdk-for-go/pkg/polaris/graphql/core"
+	polarislog "github.com/rubrikinc/rubrik-polaris-sdk-for-go/pkg/polaris/log"
+)
+
+// Example showing how to manage an AWS account with the Polaris Go SDK.
+//
+// The Polaris service account key file identifying the Polaris account should
+// either be placed at ~/.rubrik/polaris-service-account.json or pointed out by
+// the RUBRIK_POLARIS_SERVICEACCOUNT_FILE environment variable.
+func main() {
+	ctx := context.Background()
+
+	// Load configuration and create client.
+	polAccount, err := polaris.DefaultServiceAccount(true)
+	if err != nil {
+		log.Fatal(err)
+	}
+	client, err := polaris.NewClientWithLogger(
+		polAccount,
+		polarislog.NewStandardLogger(),
+	)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	awsClient := aws.Wrap(client)
+
+	// RSC features and their permission groups.
+	features := []core.Feature{
+		core.FeatureOutpost.WithPermissionGroups(core.PermissionGroupBasic),
+	}
+
+	// Add the AWS default account to Polaris. Usually resolved using the
+	// environment variables AWS_ACCESS_KEY_ID, AWS_SECRET_ACCESS_KEY and
+	// AWS_DEFAULT_REGION.
+	id, err := awsClient.AddAccount(
+		ctx,
+		aws.Default(),
+		features,
+		aws.Regions("us-east-2"),
+		aws.OutpostAccountID("123456789012"),
+	)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// List the AWS accounts added to Polaris.
+	account, err := awsClient.Account(
+		ctx,
+		aws.CloudAccountID(id),
+		core.FeatureOutpost,
+	)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Printf("Name: %v, NativeID: %v\n", account.Name, account.NativeID)
+	for _, feature := range account.Features {
+		fmt.Printf(
+			"Feature: %v, Regions: %v, Status: %v\n",
+			feature.Name,
+			feature.Regions,
+			feature.Status,
+		)
+	}
+
+	// Remove the AWS account from Polaris.
+	err = awsClient.RemoveAccount(
+		ctx,
+		aws.Default(),
+		[]core.Feature{core.FeatureOutpost},
+		false,
+	)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
