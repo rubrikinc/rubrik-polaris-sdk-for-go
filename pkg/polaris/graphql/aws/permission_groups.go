cfee862f51239de2487d8e6d30a56b44d908359e
Pontus Leitzler <leitzler@gmail.com>
2025-12-18 09:10:07 +0100

    Add permission group queries

---
+++ pkg/polaris/graphql/aws/permission_groups.go
@@ -0,0 +1,67 @@
+// Copyright 2025 Rubrik, Inc.
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to
+// deal in the Software without restriction, including without limitation the
+// rights to use, copy, modify, merge, publish, distribute, sublicense, and/or
+// sell copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+// FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+// DEALINGS IN THE SOFTWARE.
+
+package aws
+
+import (
+	"context"
+	"encoding/json"
+
+	"github.com/rubrikinc/rubrik-polaris-sdk-for-go/pkg/polaris/graphql"
+	"github.com/rubrikinc/rubrik-polaris-sdk-for-go/pkg/polaris/graphql/core"
+	"github.com/rubrikinc/rubrik-polaris-sdk-for-go/pkg/polaris/log"
+)
+
+// PermissionGroupInfo holds information about a permission group.
+type PermissionGroupInfo struct {
+	PermissionGroup core.PermissionGroup `json:"permissionsGroup"`
+	Version         int                  `json:"version"`
+}
+
+// FeaturePermissionGroups holds the permission groups for a feature.
+type FeaturePermissionGroups struct {
+	Feature           string                `json:"feature"`
+	PermissionsGroups []PermissionGroupInfo `json:"permissionsGroupPermissions"`
+}
+
+// AllPermissionsGroupsByFeature returns the permission groups for the specified
+// features.
+func (a API) AllPermissionsGroupsByFeature(ctx context.Context, features []core.Feature) ([]FeaturePermissionGroups, error) {
+	a.log.Print(log.Trace)
+
+	query := allAwsLatestPermissionsByPermissionsGroupQuery
+	buf, err := a.GQL.Request(ctx, query, struct {
+		Features []string `json:"features"`
+	}{Features: core.FeatureNames(features)})
+	if err != nil {
+		return nil, graphql.RequestError(query, err)
+	}
+
+	var payload struct {
+		Data struct {
+			Result []FeaturePermissionGroups `json:"result"`
+		} `json:"data"`
+	}
+	if err := json.Unmarshal(buf, &payload); err != nil {
+		return nil, graphql.UnmarshalError(query, err)
+	}
+
+	return payload.Data.Result, nil
+}
